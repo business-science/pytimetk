53fd66fcf2e5013e4e34e17b048ea6912e89d2e8
Matt Dancho <mdancho@gmail.com>
2023-09-18 15:51:54 -0400

    use importlib.resources

--- src/timetk/data.py
+++ src/timetk/data.py
@@ -1,16 +1,48 @@
-import pkg_resources
-import pandas as pd
 
-def load_dataset(name = "m4_daily", **kwargs):
-    """[summary]
+import pandas as pd
+from importlib.resources import open_text
+from importlib.resources import contents
+
+def load_dataset(name = "m4_daily", verbose=False, **kwargs):
+    """The `pytimetk` package comes with various time series datasets that can be loaded by name using this function. These include:
+    
+    - `m4_hourly`: The M4 hourly dataset
+    - `m4_daily`: The M4 daily dataset
+    - `m4_weekly`: The M4 weekly dataset
+    - `m4_monthly`: The M4 monthly dataset
+    - `m4_quarterly`: The M4 quarterly dataset
+    - `m4_yearly`: The M4 yearly dataset
+    - `bike_sharing_daily`: The bike sharing daily dataset
+    - `bike_sales_sample`: The bike sales sample dataset
+    - `taylor_30_min`: The Taylor 30 minute dataset
+    - `walmart_sales_weekly`: The Walmart sales weekly dataset
+    - `wikipedia_traffic_daily`: The Wikipedia traffic daily dataset
 
     Args:
-        name (str, optional): [description]. Defaults to "m4_daily".
+        name (str, optional): Name of the dataset. Defaults to "m4_daily".
+        verbose (bool, optional): Prints the names of the available datasets. Defaults to False.
+        **kwargs: Additional arguments passed to `pandas.read_csv`
+        
 
     Returns:
-        [type]: [description]
+        pandas.DataFrame: The requested dataset as a pandas DataFrame
+        
+    Example:
+    >>> import timetk
+    >>> timetk.load_dataset("m4_daily", verbose=True)
+    
     """
-
-    stream = pkg_resources.resource_stream(__name__, f'datasets/{name}.csv')
-    return pd.read_csv(stream, **kwargs)
+    
+    # Return the list of available datasets
+    if verbose:
+        file_names = list(contents("timetk.datasets"))
+        dataset_list = [name.rstrip('.csv') for name in file_names]
+        print("Available Datasets:")
+        print(dataset_list)
+    
+    # Load the dataset
+    with open_text("timetk.datasets", f"{name}.csv") as f:
+        df = pd.read_csv(f, **kwargs)
+        
+    return df
 
