91fca09455b7fa8c32f6cfad5509f6d1ea15cf8b
Matt Dancho <mdancho@gmail.com>
2023-09-19 17:21:47 -0400

    switch to Numpy format

--- src/timetk/summarize_by_time.py
+++ src/timetk/summarize_by_time.py
@@ -18,56 +18,52 @@ def summarize_by_time(
     *args,
     **kwargs
 ) -> pd.DataFrame:
-    """
-    Applies one or more aggregating functions by a Pandas Period
-    or TimeStamp to one or more numeric columns.
-
-    Args:
-        data (pd.DataFrame): 
-            A pandas data frame with a date column and value
-            column.
-        date_column (str):
-            The name of a single date or datetime column to be 
-            aggregate by. Must be datetime64.
-        value_column (str or list): 
-            The names of one or more value columns to be 
-            aggregated by.
-        groups (str or list or None, optional):
-            One or moe column names representing groups to 
-            aggregate by. Defaults to None.
-        rule (str, optional): 
-            A pandas frequency (offset) such as "D" for Daily or 
-            "MS" for month start. Defaults to "D".
-        agg_func (function or list, optional):
-            One or more aggregating functions such as np.sum.
-            Defaults to np.sum.
-        kind (str, optional):
-            One of "timestamp" or "period". Defaults to "timestamp".
-        wide_format (bool, optional): 
-            Whether or not to return "wide" of "long format.
-            Defaults to False.
-        fillna (int, optional): 
-            Value to fill missing data. Defaults to 0.
-            If missing values are desired, use np.nan.
-        flatten_column_names (bool, optional):
-            Whether or not to flatten the multiindex column
-            names. Defaults to True.
-        reset_index (bool, optional):
-            Whether or not to reset the index. Defaults to True.
-        *args, **kwargs:
-            Arguments passed to pd.DataFrame.agg()
-
-    Returns:
-        pd.DataFrame: A data frame that is summarized
-            by time.
-            
-    Examples:
+    '''The `summarize_by_time` function aggregates data by a specified time period and one or more numeric
+    columns, allowing for grouping and customization of the aggregation.
+    
+    Parameters
+    ----------
+    data : pd.DataFrame or pd.core.groupby.generic.DataFrameGroupBy
+        A pandas DataFrame or a pandas GroupBy object. This is the data that you want to summarize by time.
+    date_column : str
+        The name of the column in the data frame that contains the dates or timestamps to be aggregated by. This column must be of type datetime64.
+    value_column : str or list
+        The `value_column` parameter is the name of one or more columns in the DataFrame that you want to aggregate by. It can be either a string representing a single column name, or a list of strings representing multiple column names.
+    groups : str or list or None
+        The `groups` parameter is an optional parameter that allows you to specify one or more column names representing groups to aggregate by. If you want to aggregate the data by specific groups, you can pass the column names as a string or a list to the `groups` parameter. If you want to aggregate the data without grouping, you can set `groups` to None. The default value is None.
+    rule : str, optional
+        The `rule` parameter specifies the frequency at which the data should be aggregated. It accepts a string representing a pandas frequency offset, such as "D" for daily or "MS" for month start. The default value is "D", which means the data will be aggregated on a daily basis.
+    agg_func : list, optional
+        The `agg_func` parameter is used to specify one or more aggregating functions to apply to the value column(s) during the summarization process. It can be a single function or a list of functions. The default value is `"sum"`, which represents the sum function. 
+    kind : str, optional
+        The `kind` parameter specifies whether the time series data is represented as a "timestamp" or a "period". If `kind` is set to "timestamp", the data is treated as a continuous time series with specific timestamps. If `kind` is set to "period", the data is treated as a discrete time series with specific periods. The default value is "timestamp".
+    wide_format : bool, optional
+        A boolean parameter that determines whether the output should be in "wide" or "long" format. If set to `True`, the output will be in wide format, where each group is represented by a separate column. If set to False, the output will be in long format, where each group is represented by a separate row. The default value is `False`.
+    fillna : int, optional
+        The `fillna` parameter is used to specify the value to fill missing data with. By default, it is set to 0. If you want to keep missing values as NaN, you can use `np.nan` as the value for `fillna`.
+    flatten_column_names : bool, optional
+        A boolean parameter that determines whether or not to flatten the multiindex column names. If set to `True`, the multiindex column names will be flattened. If set to `False`, the multiindex column names will be preserved. The default value is `True`.
+    reset_index : bool, optional
+        A boolean parameter that determines whether or not to reset the index of the resulting DataFrame. If set to True, the index will be reset to the default integer index. If set to False, the index will not be reset. The default value is True.
+    
+    Returns
+    -------
+        a Pandas DataFrame that is summarized by time.
+        
+    Examples
+    --------
+    ```{python}
     import timetk
+    from timetk import load_dataset
     import pandas as pd
     
-    df = timetk.data.load_dataset('bike_sales_sample')
+    df = load_dataset('bike_sales_sample')
     df['order_date'] = pd.to_datetime(df['order_date'])
     
+    df
+    ```
+    
+    ```{python}
     # Summarize by time with a DataFrame object
     ( 
         df 
@@ -80,7 +76,9 @@ def summarize_by_time(
                 agg_func = ['mean', 'sum']
             )
     )
+    ```
     
+    ```{python}
     # Summarize by time with a GroupBy object
     (
         df 
@@ -91,7 +89,11 @@ def summarize_by_time(
                 wide_format = True,
             )
     )
-    """
+    ```
+    
+    '''
+    
+    
     
     # Check if data is a Pandas DataFrame
     if not isinstance(data, pd.DataFrame):
@@ -148,3 +150,4 @@ def summarize_by_time(
 
 # Monkey patch the summarize_by_time method to pandas groupby objects
 pd.core.groupby.generic.DataFrameGroupBy.summarize_by_time = summarize_by_time
+
