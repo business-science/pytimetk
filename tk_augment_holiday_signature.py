5af85fd7c2c42b316545d58094f7111f34147237
JB Kurland <65054169+JustinKurland@users.noreply.github.com>
2023-09-24 10:02:19 -0400

    tk_augment_holiday_signature.py
    This is largely a port of the original tk_augment_holiday_signature from the timetk in R, but with a greater number of holidays, plus the addition of a day before the holiday and day after the holiday indicator.

--- tk_augment_holiday_signature.py
+++ tk_augment_holiday_signature.py
@@ -1,25 +1,28 @@
 # Dependencies
 import pandas as pd
 import numpy as np
-import datetime as dt
-import math 
-import pandas_flavor as pf
 import holidays
-
+import math
+import pandas_flavor as pf
+from typing import Union
 
 @pf.register_dataframe_method
-def tk_augment_holiday_signature(x, country = holidays.UnitedStates):
-    """Engineers 3 different holiday features from a single datetime for 80+ countries.
+def tk_augment_holiday_signature(
+    df: pd.DataFrame,
+    date_col: Union[str, pd.Series],
+    country_name: str = 'UnitedStates'
+) -> pd.DataFrame:
+    """
+    Engineers three different holiday features from a single datetime for 80+ countries.
 
     Args:
-        x ([datetime64[ns]]): 
-            A datatime64[ns] dtype column.
-        country (module from holidays package, optional): 
-            This parameter must always follow the naming pattern 'holidays.CountryName' 
-            Defaults to United States holidays, but the following countries are currently 
-            available and accessible by the full name or ISO code:
+        df (pd.DataFrame): The input DataFrame.
+        date_col (str or pd.Series): The name of the datetime-like column in the DataFrame.
+        country_name (str): The name of the country for which to generate holiday features. 
+                            Defaults to United States holidays, but the following countries are currently 
+                            available and accessible by the full name or ISO code:
             
-            Any of the Following Are Acceptable Keys for 'CountryName'
+            Any of the following are acceptable keys for country_name
 
             Available Countries:    Full Country, Abrv. #1,   #2,   #3
             Angola:                 Angola,             AO,   AGO, 
@@ -61,7 +64,7 @@ def tk_augment_holiday_signature(x, country = holidays.UnitedStates):
             Isle Of Man:            IsleOfMan,
             Israel:                 Israel,             IL,   ISR,
             Italy:                  Italy,              IT,   ITA,
-            Jamaica:                Jamaica,            JM,  JAM,
+            Jamaica:                Jamaica,            JM,   JAM,
             Japan:                  Japan,              JP,   JPN,
             Kenya:                  Kenya,              KE,   KEN,
             Korea:                  Korea,              KR,   KOR,
@@ -76,7 +79,7 @@ def tk_augment_holiday_signature(x, country = holidays.UnitedStates):
             Netherlands:            Netherlands,        NL,   NLD,
             NewZealand:             NewZealand,         NZ,   NZL,
             Nicaragua:              Nicaragua,          NI,   NIC,
-            Nigeria:                Nigeria,            NG,  NGA,
+            Nigeria:                Nigeria,            NG,   NGA,
             Northern Ireland:       NorthernIreland,
             Norway:                 Norway,             NO,   NOR,
             Paraguay:               Paraguay,           PY,   PRY,
@@ -101,51 +104,62 @@ def tk_augment_holiday_signature(x, country = holidays.UnitedStates):
             United Arab Emirates:   UnitedArabEmirates, AE,   ARE,
             United Kingdom:         UnitedKingdom,      GB,   GBR,   UK,
             United States:          UnitedStates,       US,   USA,
-            Venezuala:              Venezuala,          YV,   VEN,
+            Venezuela:              Venezuela,          YV,   VEN,
             Vietnam:                Vietnam,            VN,   VNM,
             Wales:                  Wales
 
     Returns:
-        DataFrame: A pandas data frame that leverages the date to generate three holiday specific features:
-            - holiday: A logical (0,1) feature that captures if the day is an official national holiday
-            - before_holiday: A logical (0,1) feature that captures if the day is the day before an official national holiday
-            - after_holiday: A logical (0,1) feature that captures if the day is the day after an official national holiday
+        pd.DataFrame: A pandas DataFrame with three holiday-specific features.
     """
+    
+    # Ensure the date column exists in the DataFrame
+    if date_col not in df.columns:
+        raise ValueError(f"'{date_col}' not found in DataFrame columns.")
+    
+    # Extract start and end years directly from the Series
+    start_year = df[date_col].min().year
+    end_year = df[date_col].max().year
 
-    # Start Timestamp of Series
-    start = pd.to_datetime(x.min())
-
-    # End Time Stamp of the Series
-    end = pd.to_datetime(x.max())
-
-    # Start Year of the Series
-    start_year = start.year
-
-    # End Year of the Series
-    end_year = end.year
-
-    # Create a List of Years (Integers) from start year to end year
+    # Create a list of years (integers) from start year to end year
     years = list(range(math.ceil(start_year), math.floor(end_year) + 1))
-
-    # Create Dataframe of Full Length of the Series
-    df = pd.DataFrame({'Dates':pd.date_range(start, end)})
-
-    # Create Empty Holiday List
+    
+    # Check if valid years were found
+    if not years:
+        raise ValueError("No valid years found for holiday calculations.")
+    
+    # Create a DataFrame of the full length of the Series
+    date_range = pd.date_range(df[date_col].min(), df[date_col].max())
+    holiday_df = pd.DataFrame({'date': date_range})
+    
+    # Create an empty list to store holidays
     series_holidays = []
 
-    # Append Holidays from Selected Country to List
-    for date in country(years=years).items():
+    # Retrieve the corresponding country's module using regular expressions
+    for key in holidays.__dict__.keys():
+        if key.lower() == country_name.lower():
+            country_module = holidays.__dict__[key]
+            break
+    else:
+        raise ValueError(f"Country '{country_name}' not found in holidays package.")
+
+    # Append holidays from the selected country to the list
+    for date in country_module(years=years).items():
         series_holidays.append(str(date[0]))
 
-    # Add Boolean (0,1) Indicator for Holiday to Series
-    df['holiday'] = [
-        1 if str(val).split()[0] in series_holidays else 0 for val in df['Dates']
-        ]
+    # Add (0, 1) indicator for holiday to the DataFrame
+    holiday_df['holiday'] = holiday_df['date'].dt.strftime('%Y-%m-%d').isin(series_holidays).astype(int)
 
-    # Add Boolean (0,1) Indicator Day Before Holiday (Lead)
-    df['before_holiday'] = df.holiday.shift(-1)
+    # Add (0, 1) indicators for day before and day after holiday
+    holiday_df['before_holiday'] = holiday_df['holiday'].shift(1).fillna(0).astype(int)
+    holiday_df['after_holiday'] = holiday_df['holiday'].shift(-1).fillna(0).astype(int)
 
-    # Add Booleaan (0,1) Indicator for Day After Holiday (Lag)
-    df['after_holiday'] = df.holiday.shift(1)
+    # Merge the two DataFrames on the 'date' column with an outer join
+    merged_df = df.merge(holiday_df, left_on=date_col, right_on='date', how='outer')
 
-    return df
+    # Drop the 'date' column
+    merged_df = merged_df.drop(columns=['date'])
+
+    # Fill NaN values in columns 'holiday', 'before_holiday', and 'after_holiday' with 0
+    merged_df[['holiday', 'before_holiday', 'after_holiday']] = merged_df[['holiday', 'before_holiday', 'after_holiday']].fillna(0).astype(int)
+
+    return merged_df
