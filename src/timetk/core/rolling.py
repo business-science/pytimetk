8d656ffa819154232e01cae04b2cc6f964a50db4
Matt Dancho <mdancho@gmail.com>
2023-10-01 20:43:54 -0400

    #66 getting really close to rolling regression

--- src/timetk/core/rolling.py
+++ src/timetk/core/rolling.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import pandas_flavor as pf
 
-from typing import Union
-    
+from typing import Union, Optional, Callable, Tuple, List
+
 @pf.register_dataframe_method
 def augment_rolling(
     data: Union[pd.DataFrame, pd.core.groupby.generic.DataFrameGroupBy], 
     date_column: str, 
     value_column: Union[str, list], 
-    window: Union[int, tuple, list], 
-    window_func: Union[str, list] = 'mean',
+    independent_variable_columns: Optional[Union[str, list]] = None,
+    window: Union[int, tuple, list] = 2, 
+    window_func: Union[str, list, Tuple[str, Callable]] = 'mean',
     center: bool = False,
     **kwargs,
 ) -> pd.DataFrame:
@@ -88,13 +89,68 @@ def augment_rolling(
     )
     rolled_df 
     ```
+    
+    ```{python}
+    # Rolling Regression: Using independent variables
+    # Requires: scikit-learn
+    from sklearn.linear_model import LinearRegression
+
+    def rolling_regression(df, y, X):
+    
+        model = LinearRegression()
+        X = df[X]  # Extract x values
+        y = df[y]  # Extract y values
+        model.fit(X, y)
+        ret = pd.Series([model.intercept_, model.coef_[0]], index=['Intercept', 'Slope'])
+        
+        return [ret]
+        
+    df = pd.DataFrame({
+        'id': [1, 1, 1, 2, 2, 2],
+        'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05', '2023-01-06']),
+        'value1': [10, 20, 30, 40, 50, 60],
+        'value2': [5, 15, 25, 35, 45, 55],
+        'value3': [2, 4, 6, 8, 10, 12]
+    })
+
+    # Example to call the function
+    result_df = (
+        df.groupby('id')
+        .augment_rolling(
+            date_column='date',
+            value_column='value1',
+            independent_variable_columns=['value2', 'value3'],
+            window=2,
+            window_func=[('regression', rolling_regression)]
+        )
+    )
+
+    result_df
+
+
+    ```
     '''
     
     
-    # Check if data is a Pandas DataFrame or GroupBy object
-    if not isinstance(data, pd.DataFrame):
-        if not isinstance(data, pd.core.groupby.generic.DataFrameGroupBy):
-            raise TypeError("`data` is not a Pandas DataFrame.")
+    def rolling_apply(func, series, *args):        
+        result = series.rolling(window=window_size, min_periods=window_size, center=center, **kwargs).apply(lambda x: func(x, *args), raw=False)
+        return result
+    
+    def rolling_apply_2(func, df, *args):
+        
+        results = []
+        for start in range(len(df) - window_size + 1):
+            window_df = df.iloc[start:start + window_size]
+            result = func(window_df, *args, **kwargs)
+            results.append(result)
+        
+        ret = pd.DataFrame(results, index=df.index[window_size - 1:])
+
+        return ret
+    
+    
+    if not isinstance(data, (pd.DataFrame, pd.core.groupby.generic.DataFrameGroupBy)):
+        raise TypeError("`data` must be a Pandas DataFrame or GroupBy object.")
         
     if isinstance(value_column, str):
         value_column = [value_column]
@@ -103,42 +159,42 @@ def augment_rolling(
         window = [window]
     elif isinstance(window, tuple):
         window = list(range(window[0], window[1] + 1))
-    elif not isinstance(window, list):
-        raise TypeError(f"Invalid window type: {type(window)}. Please use int, tuple, or list.")
         
-    if isinstance(window_func, str):
+    if isinstance(window_func, (str, tuple)):
         window_func = [window_func]
-        
-    if isinstance(data, pd.DataFrame):
-        data = data.copy()
-        group_names = None
-        group_df = data.sort_values(by=[date_column])
-        grouped = [([], data)]
-    else: 
+    
+    data_copy = data.copy() if isinstance(data, pd.DataFrame) else data.obj.copy()
+    
+    if isinstance(data, pd.core.groupby.generic.DataFrameGroupBy):
         group_names = data.grouper.names
-        grouped = data.obj.copy()
-        grouped = grouped.sort_values(by=[*group_names, date_column]).groupby(group_names)           
+        grouped = data_copy.sort_values(by=[*group_names, date_column]).groupby(group_names)
+    else: 
+        group_names = None
+        grouped = [([], data_copy.sort_values(by=[date_column]))]
     
     result_dfs = []
-    
     for _, group_df in grouped:
         for value_col in value_column:
             for window_size in window:
                 for func in window_func:
-                    
                     if isinstance(func, tuple):
-                        
                         func_name, func = func
-                        
                         new_column_name = f"{value_col}_rolling_{func_name}_win_{window_size}"
                         
-                        group_df[new_column_name] = group_df[value_col].rolling(window=window_size, min_periods=1,center=center, **kwargs).apply(func, raw=True)
-                    
+                        if independent_variable_columns:
+                            if isinstance(independent_variable_columns, str):
+                                independent_variable_columns = [independent_variable_columns]
+                            
+                            # Here, pass window of rows to the custom function
+                            independent_vars = group_df[independent_variable_columns]
+                            
+                            group_df[new_column_name] = rolling_apply_2(func, group_df, value_col, independent_variable_columns)
+                        else:
+                            group_df[new_column_name] = rolling_apply(func, group_df[value_col])
+                            
                     elif isinstance(func, str):
-                        
                         new_column_name = f"{value_col}_rolling_{func}_win_{window_size}"
-                        
-                        rolling_method = getattr(group_df[value_col].rolling(window=window_size, min_periods=1,center=center, **kwargs), func, None)
+                        rolling_method = getattr(group_df[value_col].rolling(window=window_size, min_periods=1, center=center, **kwargs), func, None)
                         
                         if rolling_method:
                             group_df[new_column_name] = rolling_method()
@@ -147,13 +203,10 @@ def augment_rolling(
                     
                     else:
                         raise TypeError(f"Invalid function type: {type(func)}")
-        
+                    
         result_dfs.append(group_df)
 
-    if group_names is None:
-        result_df = pd.concat(result_dfs).sort_values(by=[date_column])
-    else:
-        result_df = pd.concat(result_dfs).sort_values(by=[*group_names, date_column])
+    result_df = pd.concat(result_dfs).sort_index()  # Sort by the original index
     
     return result_df
 
