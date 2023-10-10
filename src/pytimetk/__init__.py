834eddff86ab73ad7cf395efffe24d71fc87aad0
Matt Dancho <mdancho@gmail.com>
2023-10-10 11:40:25 -0400

    add parallel_apply to docs

--- src/pytimetk/__init__.py
+++ src/pytimetk/__init__.py
@@ -90,6 +90,9 @@ from .utils.memory_helpers import (
 from .utils.pandas_helpers import (
     flatten_multiindex_column_names, glimpse
 )
+from .utils.parallel_helpers import (
+    parallel_apply
+)
 
 
 
