aa92023492c24f46663b79c03dfddb5f80e9ef46
Matt Dancho <mdancho@gmail.com>
2023-10-09 20:29:21 -0400

    automatic anomaly detection

--- src/pytimetk/core/anomaly.py
+++ src/pytimetk/core/anomaly.py
@@ -2,9 +2,10 @@ import pandas as pd
 import pandas_flavor as pf
 import numpy as np
 
-from typing import Union
+from typing import Union, Optional
 
 from pytimetk.utils.checks import check_dataframe_or_groupby, check_date_column, check_value_column
+from pytimetk.core.frequency import get_frequency, get_seasonal_frequency, get_trend_frequency
 
 from statsmodels.tsa.seasonal import STL
 from statsmodels.tsa.seasonal import seasonal_decompose
@@ -14,20 +15,23 @@ def anomalize(
     data: Union[pd.DataFrame, pd.core.groupby.generic.DataFrameGroupBy],
     date_column: str,
     value_column: str,
-    period: int = None,
+    period: Optional[int] = None,
+    trend: Optional[int] = None,
     method: str = 'twitter',
     decomp: str = 'additive',
     clean: str = 'linear',
     iqr_alpha: float = 0.05,
     max_anomalies: float = 0.2,
-    bind_data = False,
-    
+    bind_data: bool = False,
+    verbose = False,
 ) -> pd.DataFrame:
     """
     
     Examples
     --------
     ``` {python}
+    # The code is importing the `pytimetk` library as `tk`, as well as the `pandas` and `numpy`
+    # libraries.
     import pytimetk as tk
     import pandas as pd
     import numpy as np
@@ -48,7 +52,14 @@ def anomalize(
     df.plot_timeseries("date", "value")
     
     # Anomalize the data
-    anomalize_df = tk.anomalize(df, "date", "value", method = "twitter", iqr_alpha = 0.05)
+    anomalize_df = tk.anomalize(
+        df, "date", "value",
+        # period = 12,
+        # trend = 12*2, 
+        method = "twitter", 
+        iqr_alpha = 0.05,
+        verbose = True,
+    )
     
     anomalize_df.glimpse()
     
@@ -58,12 +69,15 @@ def anomalize(
         .groupby("variable") \
         .plot_timeseries("date", "val", color_column = "variable", smooth = False)
     
+    # Visualize the anomaly bands
     anomalize_df[["date", "observed", "recomposed_l1", "recomposed_l2"]] \
         .melt(id_vars = "date", value_name='val') \
         .plot_timeseries("date", "val", color_column = "variable", smooth = False)
-        
+    
+    # Get the anomalies    
     anomalize_df.query("anomaly=='Yes'")
         
+    # Visualize observed vs cleaned
     anomalize_df[["date", "observed", "observed_clean"]] \
         .melt(id_vars = "date", value_name='val') \
         .plot_timeseries("date", "val", color_column = "variable", smooth = False)
@@ -75,26 +89,51 @@ def anomalize(
     check_date_column(data, date_column)
     check_value_column(data, value_column)
     
+    orig_date_column = data[date_column]
+    
+    data = data.copy()
+    
+    # STEP 0: Get the seasonal period and trend frequency
+    if period is None:
+        
+        period = get_seasonal_frequency(data[date_column], numeric=True)
+        
+        period = int(period)
+        
+        if verbose:
+            print(f"Using seasonal frequency of {period} observations")
+    
+    
+    if trend is None:
+            
+        trend = get_trend_frequency(data[date_column], numeric=True)
+        
+        trend = int(trend)
+        
+        if verbose:
+            print(f"Using trend frequency of {trend} observations")
+    
     # STEP 1: Decompose the time series
     if method == 'twitter':
-        
+                    
+        median_span = np.round(len(data) / trend)
+        median_span = int(median_span)
+
         result = _twitter_decompose(
             data = data, 
             date_column=date_column, 
             value_column=value_column, 
             period=period,
-            median_span=None,
-            model=decomp
+            median_span=median_span, 
+            model=decomp,
         )
     else:
         result = _seasonal_decompose(
-            data = data, 
+            data=data, 
             date_column=date_column, 
             value_column=value_column, 
-            period = period,
+            period=period,
             model=decomp,
-            filt=None,
-            two_sided=True, 
             extrapolate_trend = 'freq'
         )
     
@@ -121,13 +160,71 @@ def anomalize(
     result['observed_clean'] = result['observed'] \
         .where(result['anomaly']=='No', np.nan) \
         .interpolate(method=clean, limit_direction='both')
+        
+        
+    result[date_column] = orig_date_column
     
     # STEP 5: Bind the data
     if bind_data:
         result = pd.concat([data, result.drop(date_column, axis=1)], axis=1)
     
     return result
+
+ 
+def _twitter_decompose(
+    data, 
+    date_column, 
+    value_column, 
+    period = None, 
+    median_span = None,
+    model = 'additive',
+):
+    orig_index = data.index
+        
+    series = data.set_index(date_column)[value_column]
+     
     
+    # Need to add freq, trend, and kwargs
+    result = seasonal_decompose(
+        series, 
+        period=period,
+        model=model,
+        extrapolate_trend='freq',
+    )
+    
+    # Construct TS Decomposition DataFrame
+    observed = series
+    
+    seasadj = series - result.seasonal
+    seasadj.name = 'seasadj'
+    
+    # Calculate median trend
+    if median_span is None:
+        median_span = 4
+    
+    def repeat_sequence(seq, length_out):
+        quotient, remainder = divmod(length_out, len(seq))
+        return seq * quotient + seq[:remainder]
+    
+    df = pd.DataFrame(seasadj)
+    
+    df['median_index'] = sorted(repeat_sequence(list(range(median_span)), len(seasadj)))
+    
+    trend = df.groupby('median_index')['seasadj'].transform('median')
+    
+    resid = seasadj - trend
+    
+    
+    result_df = pd.concat([observed, result.seasonal, seasadj, trend, resid], axis=1)
+    
+    result_df.columns = ['observed', 'seasonal', 'seasadj', 'trend', 'remainder']
+    
+    result_df.reset_index(inplace=True)
+    
+    result_df.index = orig_index
+    
+    return result_df 
+     
 
 def _seasonal_decompose(
     data, 
@@ -181,8 +278,7 @@ def _seasonal_decompose(
     trend = result.trend
     
     resid = seasadj - trend
-    
-    
+        
     result_df = pd.concat([observed, result.seasonal, seasadj, trend, resid], axis=1)
     
     result_df.columns = ['observed', 'seasonal', 'seasadj', 'trend', 'remainder']
@@ -191,10 +287,10 @@ def _seasonal_decompose(
     
     result_df.index = orig_index
 
-    
     return result_df 
 
-def _stl_decompose(data, date_column, value_column, period = None, seasonal_span = None, trend_span = None, robust = True, **kwargs):
+def _stl_decompose(
+    data, date_column, value_column, period = None, seasonal_span = None, trend_span = None, robust = True, **kwargs):
     '''
     This is an internal function that is not meant to be called directly by the user.
     
@@ -245,62 +341,7 @@ def _stl_decompose(data, date_column, value_column, period = None, seasonal_span
 
     
     return result_df 
-    
-def _twitter_decompose(
-    data, 
-    date_column, 
-    value_column, 
-    period = None, 
-    median_span = None,
-    model = 'additive',
-):
-    orig_index = data.index
-        
-    series = data.set_index(date_column)[value_column]
-     
-    
-    # Need to add freq, trend, and kwargs
-    result = seasonal_decompose(
-        series, 
-        period=period,
-        model=model,
-        extrapolate_trend='freq',
-    )
-    
-    # Construct TS Decomposition DataFrame
-    observed = series
-    
-    seasadj = series - result.seasonal
-    seasadj.name = 'seasadj'
-    
-    # Calculate median trend
-    if median_span is None:
-        median_span = 4
-    
-    def repeat_sequence(seq, length_out):
-        quotient, remainder = divmod(length_out, len(seq))
-        return seq * quotient + seq[:remainder]
-    
-    df = pd.DataFrame(seasadj)
-    
-    df['median_index'] = sorted(repeat_sequence(list(range(median_span)), len(seasadj)))
-    
-    trend = df.groupby('median_index')['seasadj'].transform('median')
-    
-    resid = seasadj - trend
-    
-    
-    result_df = pd.concat([observed, result.seasonal, seasadj, trend, resid], axis=1)
-    
-    result_df.columns = ['observed', 'seasonal', 'seasadj', 'trend', 'remainder']
-    
-    result_df.reset_index(inplace=True)
-    
-    result_df.index = orig_index
-
-    
-    return result_df 
-    
+   
     
     
     
