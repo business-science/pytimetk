e42746cc3a1dc19a5a28b4a467e253e2fe349780
JB Kurland <65054169+JustinKurland@users.noreply.github.com>
2023-10-21 10:46:21 -0400

    Update plot_anomaly_decomp.py docstring pep8
    plot_anomaly_decomp.py docstring pep8

--- src/pytimetk/plot/plot_anomaly_decomp.py
+++ src/pytimetk/plot/plot_anomaly_decomp.py
@@ -32,49 +32,81 @@ def plot_anomaly_decomp(
 
     engine: str = 'plotly',    
 ):
-    '''The `plot_anomaly_decomp` function takes in data from the `anomalize()` function, and
-    returns a plot of the anomaly decomposition.
+    '''
+    The `plot_anomaly_decomp` function takes in data from the `anomalize()` 
+    function, and returns a plot of the anomaly decomposition.
     
     Parameters
     ----------
     data : Union[pd.DataFrame, pd.core.groupby.generic.DataFrameGroupBy]
-        The input data for the plot from `anomalize`. It can be either a pandas DataFrame or a pandas DataFrameGroupBy object.
+        The input data for the plot from `anomalize`. It can be either a pandas 
+        DataFrame or a pandas DataFrameGroupBy object.
     date_column : str
         The name of the column in the data that contains the dates.
     line_color : str, optional
-        The color of the line in the plot. It is specified as a hexadecimal color code. The default value is "#2c3e50".
+        The color of the line in the plot. It is specified as a hexadecimal 
+        color code. The default value is "#2c3e50".
     line_size : Optional[float]
-        The line_size parameter determines the thickness of the lines in the plot. It is an optional parameter, so if you don't specify a value, the default line size will be used.
+        The line_size parameter determines the thickness of the lines in the 
+        plot. It is an optional parameter, so if you don't specify a value, the 
+        default line size will be used.
     line_type : str, optional
-        The `line_type` parameter specifies the type of line to be used in the plot. It can take the following values:
+        The `line_type` parameter specifies the type of line to be used in the 
+        plot. It can take the following values:
         - "solid" (default): a solid line
         - "dashed": a dashed line
     line_alpha : float
-        The `line_alpha` parameter controls the transparency of the lines in the plot. It accepts a float value between 0 and 1, where 0 means completely transparent and 1 means completely opaque.
+        The `line_alpha` parameter controls the transparency of the lines in the 
+        plot. It accepts a float value between 0 and 1, where 0 means completely 
+        transparent and 1 means completely opaque.
     y_intercept : Optional[float]
-        The `y_intercept` parameter is an optional float value that specifies the y-coordinate of a horizontal line to be plotted on the graph. This line can be used to indicate a specific threshold or reference value. If not specified, no horizontal line will be plotted.
+        The `y_intercept` parameter is an optional float value that specifies 
+        the y-coordinate of a horizontal line to be plotted on the graph. This 
+        line can be used to indicate a specific threshold or reference value. If 
+        not specified, no horizontal line will be plotted.
     y_intercept_color : str, optional
-        The `y_intercept_color` parameter is used to specify the color of the y-intercept line on the plot. By default, it is set to `"#2c3e50"`, which is a dark blue color. You can change this parameter to any valid color code or name to change the color of the line.
+        The `y_intercept_color` parameter is used to specify the color of the 
+        y-intercept line on the plot. By default, it is set to `"#2c3e50"`, 
+        which is a dark blue color. You can change this parameter to any valid 
+        color code or name to change the color of the line.
     x_intercept : Optional[str]
-        The `x_intercept` parameter is used to specify the value on the x-axis where you want to draw a vertical line. This can be useful for highlighting a specific point or event in the data.
+        The `x_intercept` parameter is used to specify the value on the x-axis 
+        where you want to draw a vertical line. This can be useful for 
+        highlighting a specific point or event in the data.
     x_intercept_color : str, optional
-        The `x_intercept_color` parameter is used to specify the color of the vertical line representing the x-intercept on the plot. By default, it is set to "#2c3e50", which is a dark blue color. You can change this parameter to any valid color code or name to change the color of the line.
+        The `x_intercept_color` parameter is used to specify the color of the 
+        vertical line representing the x-intercept on the plot. By default, it 
+        is set to "#2c3e50", which is a dark blue color. You can change this 
+        parameter to any valid color code or name to change the color of the line.
     title : str, optional
         The title of the plot. It is set to "Anomaly Decomposition Plot" by default.
     x_lab : str
-        The x_lab parameter is used to specify the label for the x-axis of the plot. It is a string that represents the label text.
+        The x_lab parameter is used to specify the label for the x-axis of the 
+        plot. It is a string that represents the label text.
     y_lab : str
-        The `y_lab` parameter is used to specify the label for the y-axis of the plot. It is a string that represents the label text.
+        The `y_lab` parameter is used to specify the label for the y-axis of the 
+        plot. It is a string that represents the label text.
     x_axis_date_labels : str, optional
-        The `x_axis_date_labels` parameter is used to specify the format of the date labels on the x-axis of the plot. It accepts a string representing the format of the date labels. For example, "%b %Y" would display the month abbreviation and year (e.g., Jan 2019).
+        The `x_axis_date_labels` parameter is used to specify the format of the 
+        date labels on the x-axis of the plot. It accepts a string representing 
+        the format of the date labels. For example, "%b %Y" would display the 
+        month abbreviation and year (e.g., Jan 2019).
     base_size : float, optional
-        The `base_size` parameter determines the base font size for the plot. It is used to control the size of the text elements in the plot, such as axis labels, titles, and tick labels. The default value is 11, but you can adjust it to make the text larger or smaller
+        The `base_size` parameter determines the base font size for the plot. It
+        is used to control the size of the text elements in the plot, such as 
+        axis labels, titles, and tick labels. The default value is 11, but you 
+        can adjust it to make the text larger or smaller
     width : Optional[int]
-        The width parameter determines the width of the plot in pixels. It is an optional parameter, so if you don't specify a value, the plot will be displayed with the default width.
+        The width parameter determines the width of the plot in pixels. It is an 
+        optional parameter, so if you don't specify a value, the plot will be 
+        displayed with the default width.
     height : Optional[int]
-        The height parameter determines the height of the plot in pixels. It is an optional parameter, so if you don't specify a value, the plot will be displayed with a default height.
+        The height parameter determines the height of the plot in pixels. It is 
+        an optional parameter, so if you don't specify a value, the plot will be 
+        displayed with a default height.
     engine : str, optional
-        The `engine` parameter specifies the plotting engine to use. It can be set to either "plotly", "plotnine", or "matplotlib".
+        The `engine` parameter specifies the plotting engine to use. It can be 
+        set to either "plotly", "plotnine", or "matplotlib".
     
     Returns
     -------
@@ -82,7 +114,8 @@ def plot_anomaly_decomp(
     
     See Also
     --------
-    1. anomalize : Function that calculates the anomalies and formats the data for visualization.
+    1. anomalize : Function that calculates the anomalies and formats the data 
+    for visualization.
     2. plot_anomalies : Function that plots the anomalies.
     
     Examples
