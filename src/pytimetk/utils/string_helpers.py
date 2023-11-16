62aed929bd3ae40c215ab5c74ab48528c6f883a8
Matt Dancho <mdancho@gmail.com>
2023-11-15 22:15:59 -0500

    parse_freq_str2: handles compound frequency strings

--- src/pytimetk/utils/string_helpers.py
+++ src/pytimetk/utils/string_helpers.py
@@ -11,3 +11,22 @@ def parse_freq_str(freq_str):
         quantity = 1
     
     return quantity, unit
+
+
+def parse_freq_str2(freq_str):
+    # Regular expression to match patterns like '2H', '30T', etc.
+    matches = re.findall(r'(\d+)?([A-Z]+|min)', freq_str)
+    if not matches:
+        raise ValueError(f"Invalid frequency string: {freq_str}")
+    
+    # Process each match to extract quantity and unit
+    parsed = []
+    for match in matches:
+        quantity, unit = match
+        if quantity == '':
+            quantity = 1
+        else:
+            quantity = int(quantity)
+        parsed.append((quantity, unit))
+    
+    return parsed
